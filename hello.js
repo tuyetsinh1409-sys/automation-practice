d178ff863fd7c8ee43550c4cf015e88e30faff17
TuyetSinh <tuyetsinh1409@gmail.com>
2026-04-08 20:53:26 +0700

    Add sayGoodbye function

--- hello.js
+++ hello.js
@@ -2,4 +2,9 @@ function sayHello(name) {
  return "Hello, " + name + "!";
 }
 
+function sayGoodbye(name) {
+ return "Goodbye, " + name + "!";
+}
+
 console.log(sayHello("Automation Tester"));
+console.log(sayGoodbye("Manual Testing"));
